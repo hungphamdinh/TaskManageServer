ca6ca445ed2a60b449001e526a74fd5d03c825e2
Phamdinhhung <hungphamdinh100@gmail.com>
2021-06-09 21:24:51 +0700

    up

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const config = require('./config');
-// const studentRoutes = require('./routes/student-routes');
-// const userRoutes = require('./routes/user-routes');
-// const taskRoutes = require('./routes/task-routes');
-// const subTaskRoutes = require('./routes/subTask-routes');
-// const commentRoute = require('./routes/comment-route');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const config = require('./config');
+const studentRoutes = require('./routes/student-routes');
+const userRoutes = require('./routes/user-routes');
+const taskRoutes = require('./routes/task-routes');
+const subTaskRoutes = require('./routes/subTask-routes');
+const commentRoute = require('./routes/comment-route');
 const app = express();
 const PORT = process.env.PORT || 8080
 
 
-// app.use(express.json());
-// app.use(cors());
-// app.use(bodyParser.json());
+app.use(express.json());
+app.use(cors());
+app.use(bodyParser.json());
 
-// // app.use('/api', studentRoutes.routes);
-// app.use('/api', userRoutes.routes);
-// app.use('/api', taskRoutes.routes);
-// app.use('/api', subTaskRoutes.routes);
-// app.use('/api', commentRoute.routes);
+// app.use('/api', studentRoutes.routes);
+app.use('/api', userRoutes.routes);
+app.use('/api', taskRoutes.routes);
+app.use('/api', subTaskRoutes.routes);
+app.use('/api', commentRoute.routes);
 
 app.get("/", (req, res) => {
     res.send('Hêlo');
