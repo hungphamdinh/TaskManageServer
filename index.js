6bc7cdafe6e5baa2107ac6c68b803b2d4c79756e
Phamdinhhung <hungphamdinh100@gmail.com>
2021-06-09 21:17:18 +0700

    update

--- index.js
+++ index.js
@@ -11,17 +11,19 @@ const app = express();
 const PORT = process.env.PORT || 8080
 
 
-app.use(express.json());
-app.use(cors());
-app.use(bodyParser.json());
+// app.use(express.json());
+// app.use(cors());
+// app.use(bodyParser.json());
 
-// app.use('/api', studentRoutes.routes);
-app.use('/api', userRoutes.routes);
-app.use('/api', taskRoutes.routes);
-app.use('/api', subTaskRoutes.routes);
-app.use('/api', commentRoute.routes);
+// // app.use('/api', studentRoutes.routes);
+// app.use('/api', userRoutes.routes);
+// app.use('/api', taskRoutes.routes);
+// app.use('/api', subTaskRoutes.routes);
+// app.use('/api', commentRoute.routes);
 
-
+app.get("/", (req, res) => {
+    res.send('Hêlo');
+})
 
 // app.listen(config.port, () => console.log('App is listening on url http://localhost:' + config.port));
 app.listen(PORT, () => console.log('Listen at port ' + PORT));
